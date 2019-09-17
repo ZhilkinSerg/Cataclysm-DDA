8d7850b109289d18f917ace05bfad4a64702da66
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-09-17 21:05:04 +0300

    1

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -89,105 +89,96 @@ void projectile::unset_custom_explosion()
     custom_explosion.reset();
 }
 
+struct ammo_effect_data {
+
+    std::string effect_tag;
+
+    field_type_id aoe_field_type = fd_null;
+    int aoe_intensity_min = 0;
+    int aoe_intensity_max = 0;
+    int aoe_radius = 0;
+    int aoe_radius_z = 0;
+    int aoe_chance = 0;
+    int aoe_size = 0;
+    explosion_data aoe_explosion_data;
+    bool aoe_check_passable = false;
+    bool aoe_check_sees = false;
+    int aoe_check_sees_radius = 0;
+    bool do_flashbang = false;
+    bool do_emp_blast = false;
+
+    ammo_effect_data( const std::string &tag, const field_type_id &aoe_field,
+                      int aoe_intensity_min, int aoe_intensity_max, int aoe_radius, int aoe_radius_z,
+                      int aoe_chance, int aoe_size, explosion_data aoe_explosion_data = {},
+                      bool aoe_check_passable = false, bool aoe_check_sees = false, int aoe_check_sees_radius = 0,
+                      bool do_flashbang = false, bool do_emp_blast = false )
+        : effect_tag( tag )
+        , aoe_field_type( aoe_field )
+        , aoe_intensity_min( aoe_intensity_min )
+        , aoe_intensity_max( aoe_intensity_max )
+        , aoe_radius( aoe_radius )
+        , aoe_radius_z( aoe_radius_z )
+        , aoe_chance( aoe_chance )
+        , aoe_size( aoe_size )
+        , aoe_explosion_data( aoe_explosion_data )
+        , aoe_check_passable( aoe_check_passable )
+        , aoe_check_sees( aoe_check_sees )
+        , aoe_check_sees_radius( aoe_check_sees_radius )
+        , do_flashbang( do_flashbang )
+        , do_emp_blast( do_emp_blast ) {
+    }
+};
+
+static const std::vector<ammo_effect_data> ammo_effects = {
+    { ammo_effect_data( "NAPALM", fd_fire, 1, 1, 1, 0, 1, 3, explosion_data( 60, 0.7, true ) ) },
+    { ammo_effect_data( "NAPALM_BIG", fd_fire, 1, 1, 3, 0, 1, 4, explosion_data( 360, 0.8, true ) ) },
+    { ammo_effect_data( "PYROPHORIC", fd_fire, 2, 2, 3, 0, 1, 0, explosion_data( 360, 0.8, true ) ) },
+    { ammo_effect_data( "ACIDBOMB", fd_acid, 3, 3, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "TOXICGAS", fd_toxic_gas, 3, 3, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "GAS_FUNGICIDAL", fd_fungicidal_gas, 3, 3, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "GAS_INSECTICIDAL", fd_insecticidal_gas, 3, 3, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "SMOKE", fd_smoke, 3, 3, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "SMOKE_BIG", fd_smoke, 3, 3, 6, 0, 1, 0 ) },
+    { ammo_effect_data( "FLARE", fd_fire, 1, 1, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "LIGHTNING", fd_electricity, 3, 3, 1, 0, 1, 0 ) },
+    { ammo_effect_data( "PLASMA", fd_plasma,  2, 3, 1, 0, 2, 0 ) },
+    { ammo_effect_data( "EXPLOSIVE_HUGE", fd_null, 0, 0, 1, 0, 1, 4, explosion_data( 1200 ) ) },
+    { ammo_effect_data( "EXPLOSIVE_BIG", fd_null, 0, 0, 1, 0, 1, 3, explosion_data( 600 ) ) },
+    { ammo_effect_data( "EXPLOSIVE", fd_null, 0, 0, 1, 0, 1, 2, explosion_data( 360 ) ) },
+    { ammo_effect_data( "EXPLOSIVE_SMALL", fd_null, 0, 0, 1, 0, 1, 2, explosion_data( 360, 0.4 ) ) },
+    { ammo_effect_data( "FRAG", fd_null, 0, 0, 0, 0, 0, 15, explosion_data( 185, 0.8, false, shrapnel_data( 212, 0.05 ) ) ) },
+    { ammo_effect_data( "FLAME", fd_null, 0, 0, 0, 0, 0, 0 ) },
+    //TODO: sees & passable check
+    { ammo_effect_data( "MININUKE_MOD", fd_nuke_gas, 3, 3, 18, 0, 1, 1, explosion_data( 72000000 ), true, true, 3 ) },
+    { ammo_effect_data( "FLASHBANG", fd_null, 0, 0, 0, 0, 0, 0, explosion_data(), false, false, 0, true ) },
+    { ammo_effect_data( "EMP", fd_null, 0, 0, 0, 0, 0, 0, explosion_data( ), false, false, 0, false, true ) },
+};
+
 void apply_ammo_effects( const tripoint &p, const std::set<std::string> &effects )
 {
-    if( effects.count( "EXPLOSIVE_SMALL" ) > 0 ) {
-        // TODO: double-check if this is sensible.
-        explosion_handler::explosion( p, 360, 0.4 );
-    }
-
-    if( effects.count( "EXPLOSIVE" ) > 0 ) {
-        // TODO: double-check if this is sensible.
-        explosion_handler::explosion( p, 360 );
-    }
-
-    if( effects.count( "FRAG" ) > 0 ) {
-        // Same as a standard thrown frag grenade.
-        explosion_handler::explosion( p, 185, 0.8, false, 212, 0.05 );
-    }
-
-    if( effects.count( "NAPALM" ) > 0 ) {
-        explosion_handler::explosion( p, 60, 0.7, true );
-        // More intense fire near the center
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_fire, 1 );
-        }
-    }
-
-    if( effects.count( "NAPALM_BIG" ) > 0 ) {
-        explosion_handler::explosion( p, 360, 0.8, true );
-        // More intense fire near the center
-        for( auto &pt : g->m.points_in_radius( p, 3, 0 ) ) {
-            g->m.add_field( pt, fd_fire, 1 );
-        }
-    }
-
-    if( effects.count( "PYROPHORIC" ) > 0 ) {
-        explosion_handler::explosion( p, 360, 0.8, true );
-        // Extreme heat near the center of the explosion
-        for( auto &pt : g->m.points_in_radius( p, 3, 0 ) ) {
-            g->m.add_field( pt, fd_fire, 2 );
-        }
-    }
-
-    if( effects.count( "MININUKE_MOD" ) > 0 ) {
-        explosion_handler::explosion( p, 72000000 );
-        for( auto &pt : g->m.points_in_radius( p, 18, 0 ) ) {
-            if( g->m.sees( p, pt, 3 ) &&
-                g->m.passable( pt ) ) {
-                g->m.add_field( pt, fd_nuke_gas, 3 );
+    for( const ammo_effect_data &aed : ammo_effects ) {
+        if( effects.count( aed.effect_tag ) > 0 ) {
+            for( auto &pt : g->m.points_in_radius( p, aed.aoe_radius, aed.aoe_radius_z ) ) {
+                if( one_in( aed.aoe_chance ) ) {
+                    const bool check_sees = !aed.aoe_check_sees ||
+                                            ( aed.aoe_check_sees && g->m.sees( p, pt, aed.aoe_check_sees_radius ) );
+                    const bool check_passable = !aed.aoe_check_passable ||
+                                                ( aed.aoe_check_passable && g->m.passable( pt ) );
+                    if( check_sees && check_passable ) {
+                        g->m.add_field( pt, aed.aoe_field_type, rng( aed.aoe_intensity_min, aed.aoe_intensity_max ) );
+                    }
+                }
             }
         }
-    }
-
-    if( effects.count( "ACIDBOMB" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_acid, 3 );
+        if( aed.aoe_explosion_data.power > 0 ) {
+            explosion_handler::explosion( p, aed.aoe_explosion_data );
         }
-    }
-
-    if( effects.count( "EXPLOSIVE_BIG" ) > 0 ) {
-        // TODO: double-check if this is sensible.
-        explosion_handler::explosion( p, 600 );
-    }
-
-    if( effects.count( "EXPLOSIVE_HUGE" ) > 0 ) {
-        // TODO: double-check if this is sensible.
-        explosion_handler::explosion( p, 1200 );
-    }
-
-    if( effects.count( "TOXICGAS" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_toxic_gas, 3 );
+        if( aed.do_flashbang ) {
+            explosion_handler::flashbang( p );
         }
-    }
-    if( effects.count( "GAS_FUNGICIDAL" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_fungicidal_gas, 3 );
+        if( aed.do_emp_blast ) {
+            explosion_handler::emp_blast( p );
         }
-    }
-    if( effects.count( "GAS_INSECTICIDAL" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_insecticidal_gas, 3 );
-        }
-    }
-    if( effects.count( "SMOKE" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_smoke, 3 );
-        }
-    }
-    if( effects.count( "SMOKE_BIG" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 6, 0 ) ) {
-            g->m.add_field( pt, fd_smoke, 3 );
-        }
-    }
-
-    if( effects.count( "FLASHBANG" ) ) {
-        explosion_handler::flashbang( p );
-    }
-
-    if( effects.count( "EMP" ) ) {
-        explosion_handler::emp_blast( p );
     }
 
     if( effects.count( "NO_BOOM" ) == 0 && effects.count( "FLAME" ) > 0 ) {
@@ -195,44 +186,15 @@ void apply_ammo_effects( const tripoint &p, const std::set<std::string> &effects
             g->m.add_field( pt, fd_fire, 1 );
         }
     }
-
-    if( effects.count( "FLARE" ) > 0 ) {
-        g->m.add_field( p, fd_fire, 1 );
-    }
-
-    if( effects.count( "LIGHTNING" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            g->m.add_field( pt, fd_electricity, 3 );
-        }
-    }
-
-    if( effects.count( "PLASMA" ) > 0 ) {
-        for( auto &pt : g->m.points_in_radius( p, 1, 0 ) ) {
-            if( one_in( 2 ) ) {
-                g->m.add_field( pt, fd_plasma, rng( 2, 3 ) );
-            }
-        }
-    }
 }
 
-int aoe_size( const std::set<std::string> &tags )
+int max_aoe_size( const std::set<std::string> &tags )
 {
-    if( tags.count( "NAPALM_BIG" ) ||
-        tags.count( "EXPLOSIVE_HUGE" ) ) {
-        return 4;
-    } else if( tags.count( "NAPALM" ) ||
-               tags.count( "EXPLOSIVE_BIG" ) ) {
-        return 3;
-    } else if( tags.count( "EXPLOSIVE" ) ||
-               tags.count( "EXPLOSIVE_SMALL" ) ) {
-        return 2;
-    } else if( tags.count( "FRAG" ) ) {
-        return 15;
-    } else if( tags.count( "ACIDBOMB" ) ||
-               tags.count( "FLAME" ) ) {
-        return 1;
+    int aoe_size = 0;
+    for( const ammo_effect_data &aed : ammo_effects ) {
+        if( tags.count( aed.effect_tag ) > 0 ) {
+            aoe_size = std::max( aoe_size,  aed.aoe_size ) ;
+        }
     }
-
-    return 0;
+    return aoe_size;
 }
-
