4ef722001ab4eb5250c2b31ed13994fe4de269a5
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-03-18 16:51:58 +0300

    Updated version header generation script for VS

--- msvc-full-features/prebuild.cmd
+++ msvc-full-features/prebuild.cmd
@@ -5,9 +5,17 @@ cd ..\msvc-full-features
 echo Done
 
 echo Generating "version.h"...
+
+if NOT "%1"==[] (
+set VERSION=%1
+)
+
+if "%VERSION%"==[] (
 for /F "tokens=*" %%i in ('git describe --tags --always --dirty --match "[0-9]*.*"') do set VERSION=%%i
-if "%VERSION%"=="" (
+if "%VERSION%"==[] (
 set VERSION=Please install `git` to generate VERSION
 )
+)
+
 echo VERSION defined as "%VERSION%"
 >..\src\version.h echo #define VERSION "%VERSION%"
